b40c7eef804b4817a25531598a5c8a22cd63117f
AnaBykova <anabykovaca@gmail.com>
2023-07-17 21:17:21 -0600

    add get value

--- index.js
+++ index.js
@@ -13,23 +13,27 @@ class Transaction {
     this.amount  = amount;
     this.account = account;
   }
+
+  commit() {
+    this.account.balance += this.value;
+  }
 }
 
 class Deposit extends Transaction {
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount
   }
 }
 
 class Withdrawal extends Transaction {
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
   }
 }
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-const myAccount = new Account("snow-patrol");
+/*const myAccount = new Account("snow-patrol");
 
 t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
@@ -42,6 +46,18 @@ console.log('Transaction 2:', t2);
 t3 = new Deposit(120.00, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3);
-
+*/
 
 console.log('Balance:', balance);
+
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
+t1.commit();
+
+const t2 = new Withdrawal(50.00, myAccount);
+t2.commit();
+
+console.log('Ending Balance:', myAccount.balance);
